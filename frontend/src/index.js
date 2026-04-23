fe43b79684f0eb7a0c8e1500409aaf8cd8387d82
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2026-04-23 11:43:13 +0100

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'; // 引入它
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter> {/* 把它包裹在 App 外面 */}
+        <BrowserRouter basename={"/predictor"}> {/* 把它包裹在 App 外面 */}
             <App />
         </BrowserRouter>
     </React.StrictMode>
